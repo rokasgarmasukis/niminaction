229e8d5e380a00d754e6900b83a44b48d26b527a
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2022-11-12 08:31:54 +0100

    Implement database

--- Tweeter/src/database.nim
+++ Tweeter/src/database.nim
@@ -29,3 +29,63 @@ proc follow*(database: Database, follower: User, user: User) =
 proc create*(database: Database, user: User) =
   database.db.exec(sql"INSERT INTO User VALUES (?);", user.username)
 
+proc findUser*(database: Database, username: string, user: var User): bool =
+  let row = database.db.getRow(
+    sql"SELECT username FROM User WHERE username = ?;", username)
+  if row[0].len == 0: return false
+  else: user.username = row[0]
+
+  let following = database.db.getAllRows(
+    sql"SELECT followed_user FROM Following WHERE follower = ?;", username)
+  user.following = @[]
+  for row in following:
+    if row[0].len != 0:
+      user.following.add(row[0])
+    
+  return true
+
+proc findMessages*(database: Database, usernames: seq[string], limit = 10): seq[Message] =
+  result = @[]
+  if usernames.len == 0: return
+  var whereClause = " WHERE "
+  for i in 0 ..< usernames.len:
+    whereClause.add("username = ? ")
+    if i != usernames.len - 1:
+      whereClause.add("or ")
+  
+  let messages = database.db.getAllRows(
+    sql("SELECT username, time, msg FROM Message" & whereClause & "ORDER BY time DESC LIMIT " & $limit), usernames)
+  for row in messages:
+    result.add(Message(username: row[0], time: fromUnix(row[1].parseInt), msg: row[2]))
+
+# Database creation ###############
+var db = open("tweeter.db", "", "", "")
+
+proc close*(database: Database) =
+  database.db.close()
+  
+proc setup*(database: Database) =
+  database.db.exec(sql"""
+    CREATE TABLE IF NOT EXISTS User(
+      username text PRIMARY KEY
+    );
+  """)
+
+  database.db.exec(sql"""
+    CREATE TABLE IF NOT EXISTS Following(
+      follower text,
+      followed_user text,
+      PRIMARY KEY (follower, followed_user)
+      FOREIGN KEY (follower) REFERENCES User(username)
+      FOREIGN KEY (followed_user) REFERENCES User(username)
+    );
+  """)
+
+  database.db.exec(sql"""
+    CREATE TABLE IF NOT EXISTS Message(
+      username text,
+      time integer,
+      msg text NOT NULL,
+      FOREIGN KEY (username) REFERENCES User(username)
+    );
+  """)
